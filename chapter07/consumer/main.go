1a64960bc529c9c271ef79e1d206b644925fe4e0
ndaDayo <hayato711+nda@gmail.com>
2024-08-20 21:14:14 +0900

    [mv] chapter

---
+++ chapter07/consumer/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func main() {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":  "broker1:29092,broker2:29093,broker3:29094",
+		"group.id":           "FirstAppConsumerGroup",
+		"enable.auto.commit": false,
+		"auto.offset.reset":  "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = c.SubscribeTopics([]string{"first-app"}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	defer c.Close()
+
+	for count := 0; count < 300; count++ {
+
+		msg, err := c.ReadMessage(1000 * time.Millisecond)
+
+		if err != nil {
+			if kafkaError, ok := err.(kafka.Error); ok && kafkaError.Code() == kafka.ErrTimedOut {
+				continue
+			}
+			fmt.Printf("Consumer error: %v\n", err)
+			continue
+		}
+
+		fmt.Printf("key:%s, value:%s, topic:%s, partition:%d, offset:%d\n",
+			string(msg.Key), string(msg.Value), *msg.TopicPartition.Topic, msg.TopicPartition.Partition, msg.TopicPartition.Offset)
+
+		_, err = c.CommitMessage(msg)
+		if err != nil {
+			fmt.Printf("Failed to commit message: %v\n", err)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
---
+++ chapter07/producer/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func main() {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "broker1:29092,broker2:29093,broker3:29094",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer p.Close()
+
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	topic := "first-app"
+	for i := 1; i <= 100; i++ {
+		key := i
+		value := strconv.Itoa(i)
+
+		record := &kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Key:            []byte(strconv.Itoa(key)),
+			Value:          []byte(value),
+		}
+
+		err := p.Produce(record, nil)
+		if err != nil {
+			fmt.Printf("Produce failed: %v\n", err)
+		}
+	}
+
+	p.Flush(15 * 1000)
+}
